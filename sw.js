c3cfacb92b1e525a1d157c27f3aeaa891455f6bc
mariavilla9982-hue <mariavilla9982@gmail.com>
2026-09-24 09:46:54 -0300

    Add CloudentFlow service worker

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE="cloudentflow-pwa-v1";
+const STATIC=["/manifest.webmanifest","/cloudent-icon.svg"];
+self.addEventListener("install",event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC)).catch(()=>null));
+  self.skipWaiting();
+});
+self.addEventListener("activate",event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch",event=>{
+  if(event.request.method!=="GET")return;
+  event.respondWith(
+    fetch(event.request).catch(()=>caches.match(event.request))
+  );
+});
